482f106ed6b653d4c6ca940f7d534a445004c8a9
billwiliams <wnwillyndirangu@gmail.com>
2014-12-23 21:07:00 +0300

    added a a class ProxiSys to extend the Application

---
+++ app/src/main/java/com/example/ndirangu/proxisys/ProxiSys.java
@@ -0,0 +1,18 @@
+package com.example.ndirangu.proxisys;
+
+import android.app.Application;
+
+/**
+ * Created by NDIRANGU on 12/23/2014.
+ */
+public class ProxiSys extends Application {
+
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+
+    }
+
+}
